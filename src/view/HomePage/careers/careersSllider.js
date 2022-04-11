83d261dc019431f8e65daf746a3bba8a21930f24
anhnv <anhnv620@wru.vn>
2022-04-11 13:25:18 +0700

    Build UI for home page

---
+++ src/view/HomePage/careers/careersSllider.js
@@ -0,0 +1,113 @@
+import React from "react";
+import * as Rb from "react-bootstrap";
+
+const CareersSlider = () => {
+  return (
+    <Rb.Carousel>
+      <Rb.Carousel.Item>
+        <Rb.Row className="mini_card-slider">
+          <Rb.Col md={6} className="card_top-content">
+            <Rb.Card className="mini_card">
+              <p className="card_top-time">26.3 - 2022</p>
+              <h3 className="card_top-titles">
+                An exclusive information session for
+              </h3>
+              <p className="card_top-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat ut aliquip ex ea commodo
+                consequat.{" "}
+              </p>
+            </Rb.Card>
+          </Rb.Col>
+          <Rb.Col md={6} className="card_top-content">
+            <Rb.Card className="mini_card">
+              <p className="card_top-time">26.3 - 2022</p>
+              <h3 className="card_top-titles">
+                An exclusive information session for
+              </h3>
+              <p className="card_top-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat ut aliquip ex ea commodo
+                consequat.{" "}
+              </p>
+            </Rb.Card>
+          </Rb.Col>
+        </Rb.Row>
+      </Rb.Carousel.Item>
+      <Rb.Carousel.Item>
+        <Rb.Row className="mini_card-slider">
+          <Rb.Col md={6} className="card_top-content">
+            <Rb.Card className="mini_card">
+              <p className="card_top-time">26.3 - 2022</p>
+              <h3 className="card_top-titles">
+                An exclusive information session for
+              </h3>
+              <p className="card_top-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat ut aliquip ex ea commodo
+                consequat.{" "}
+              </p>
+            </Rb.Card>
+          </Rb.Col>
+          <Rb.Col md={6} className="card_top-content">
+            <Rb.Card className="mini_card">
+              <p className="card_top-time">26.3 - 2022</p>
+              <h3 className="card_top-titles">
+                An exclusive information session for
+              </h3>
+              <p className="card_top-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat ut aliquip ex ea commodo
+                consequat.{" "}
+              </p>
+            </Rb.Card>
+          </Rb.Col>
+        </Rb.Row>
+      </Rb.Carousel.Item>
+      <Rb.Carousel.Item>
+        <Rb.Row className="mini_card-slider">
+          <Rb.Col md={6} className="card_top-content">
+            <Rb.Card className="mini_card">
+              <p className="card_top-time">26.3 - 2022</p>
+              <h3 className="card_top-titles">
+                An exclusive information session for
+              </h3>
+              <p className="card_top-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat ut aliquip ex ea commodo
+                consequat.{" "}
+              </p>
+            </Rb.Card>
+          </Rb.Col>
+          <Rb.Col md={6} className="card_top-content">
+            <Rb.Card className="mini_card">
+              <p className="card_top-time">26.3 - 2022</p>
+              <h3 className="card_top-titles">
+                An exclusive information session for
+              </h3>
+              <p className="card_top-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat ut aliquip ex ea commodo
+                consequat.{" "}
+              </p>
+            </Rb.Card>
+          </Rb.Col>
+        </Rb.Row>
+      </Rb.Carousel.Item>
+    </Rb.Carousel>
+  );
+};
+
+export default CareersSlider;
