1901e0ec61f5da33ee3809f6cc929c40018ee01a
anhnv <anhnv620@wru.vn>
2022-04-15 01:52:30 +0700

    Fix Ui for mobile

--- src/view/Components/navbar/index.js
+++ src/view/Components/navbar/index.js
@@ -7,7 +7,7 @@ import Logo from "../../../assets/logo.png";
 export default function NavBarTop() {
   return (
     <Rb.Navbar bg="transparent" expand="lg" fixed="top">
-      <Rb.Container>
+      <Rb.Container className="nav_container">
         <Rb.Navbar.Brand href="#home">
           <img src={Logo} alt="logo" />
         </Rb.Navbar.Brand>
