77432cd6e2845123344bb513e1a644bfedb6a03a
anhnv <anhnv620@wru.vn>
2022-04-19 04:18:32 +0700

    Final commit

--- src/view/Components/blockable-academy/index.js
+++ src/view/Components/blockable-academy/index.js
@@ -1,37 +1,44 @@
-import React, { useContext } from 'react';
-import * as Rb from 'react-bootstrap';
-import './style.scss';
-import AcademyCard from './academy';
-import Arrow from '../../../assets/u_arrow-right.png';
-import { DataContext } from '../../../contexts/data.context';
+import React, { useContext } from "react";
+import * as Rb from "react-bootstrap";
+import "./style.scss";
+import AcademyCard from "./academy";
+import Arrow from "../../../assets/u_arrow-right.png";
+import { DataContext } from "../../../contexts/data.context";
 
-import './style.scss';
-import { Link } from 'react-router-dom';
+import "./style.scss";
+import { Link } from "react-router-dom";
 
 const BlockableAcademy = () => {
   const data = useContext(DataContext);
   return (
-    <section className='blockable_academy--wrapper' id='academy_block'>
+    <section className="blockable_academy--wrapper" id="academy_block">
       <Rb.Container>
-        <Rb.Row className='academy-title'>
+        <Rb.Row className="academy-title">
           <Rb.Col md={8} lg={8} sm={12} xl={8}>
             <Rb.Card>
               <Rb.Card.Body>
                 <Rb.Card.Title>
-                  {data.contents['Blockable academy'].title}
-                  <Rb.Button variant='link' className='academy-link' as={Link} to='academy'>
-                    <img src={Arrow} alt='arrow' />
+                  {data.contents["Blockable academy"].title}
+                  <Rb.Button
+                    variant="link"
+                    className="academy-link"
+                    as={Link}
+                    to="academy"
+                  >
+                    <img src={Arrow} alt="arrow" />
                   </Rb.Button>
                 </Rb.Card.Title>
-                <Rb.Card.Text>{data.contents['Blockable academy'].description}</Rb.Card.Text>
+                <Rb.Card.Text>
+                  {data.contents["Blockable academy"].description}
+                </Rb.Card.Text>
               </Rb.Card.Body>
             </Rb.Card>
           </Rb.Col>
         </Rb.Row>
-        <Rb.Row className='card_items-wrapper'>
-          {data.contents['Blockable academy'].items.map((e, i) => (
+        <Rb.Row className="card_items-wrapper">
+          {data.contents["Blockable academy"].items.map((e, i) => (
             <Rb.Col lg={4} md={4} sm={12} key={i}>
-              <AcademyCard title={e.description} desc={e.content} />
+              <AcademyCard title={e.title} desc={e.content} />
             </Rb.Col>
           ))}
         </Rb.Row>
--- src/view/Components/blockable-hub/index.js
+++ src/view/Components/blockable-hub/index.js
@@ -1,29 +1,35 @@
-import React, { useContext } from 'react';
-import * as Rb from 'react-bootstrap';
-import { DataContext } from '../../../contexts/data.context';
-import './style.scss';
+import React, { useContext } from "react";
+import * as Rb from "react-bootstrap";
+import { DataContext } from "../../../contexts/data.context";
+import "./style.scss";
+import Book from "../../../assets/hub-book.png";
 
 const BlockableHub = () => {
   const data = useContext(DataContext);
 
   return (
-    <section className='blockable_hub--wrapper' id='Blockable Hub'>
+    <section className="blockable_hub--wrapper" id="Blockable Hub">
       <Rb.Container>
         <Rb.Row>
-          <Rb.Col lg={6} sm={6} className='hub-title'>
-            <h2 className='blockable_hub-title'>{data.contents['Blockable Hub'].title}</h2>
-            <p className='blockable_hub-sub'>{data.contents['Blockable Hub'].description}</p>
+          <Rb.Col lg={6} sm={6} className="hub-title">
+            <h2 className="blockable_hub-title">
+              {data.contents["Blockable Hub"].title}
+            </h2>
+            <p className="blockable_hub-sub">
+              {data.contents["Blockable Hub"].description}
+            </p>
           </Rb.Col>
         </Rb.Row>
         <Rb.Row>
           <Rb.Col xl={6} lg={6} sm={12}></Rb.Col>
-          <Rb.Col xl={6} lg={6} sm={12} className='list-hub'>
+          <Rb.Col xl={6} lg={6} sm={12} className="list-hub">
             <Rb.Row>
-              {data.contents['Blockable Hub'].items.map((e, i) => (
+              {data.contents["Blockable Hub"].items.map((e, i) => (
                 <Rb.Col lg={6} sm={6} key={i}>
-                  <img src={e.img} alt='hub-img' />
-                  <h3 className='list_hub-title'>{e.description}</h3>
-                  <p className='list_hub-content'>{e.content}</p>
+                  {/* <img src={e.img} alt='hub-img' /> */}
+                  <img src={Book} alt="hub-img" />
+                  <h3 className="list_hub-title">{e.description}</h3>
+                  <p className="list_hub-content">{e.content}</p>
                 </Rb.Col>
               ))}
             </Rb.Row>
